3240d1e34f5be8183da47e574b3e9f9d60826407
itanaysharma <thisistanaysharma@gmail.com>
2023-05-23 21:55:00 +0530

    created a detials page and updated button on the all_listings.html

--- listings/urls.py
+++ listings/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # Listings Page
     path('all_listings/', views.all_listings, name='all_listings'),
     path('new_listing/', views.new_listing, name='new_listing'),
+    path('all_listings/<detail_id>/', views.detail, name='detail')
 ]
--- listings/views.py
+++ listings/views.py
@@ -22,3 +22,8 @@ def new_listing(request):
       
     context = {'form': form}
     return render(request, 'listings/new_listing.html', context)
+
+def detail(request,detail_id):
+    detail = Listings.objects.get(id=detail_id)
+    context = {'detail':detail}
+    return render(request, 'listings/detail.html', context)
