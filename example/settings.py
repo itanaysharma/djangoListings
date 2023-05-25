8febcd1f8038c0ada08130d5b4e1c981d23dfc20
itanaysharma <thisistanaysharma@gmail.com>
2023-05-25 09:17:55 +0530

    Added logout and login pages with functionality

--- example/settings.py
+++ example/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listings'
+    'listings',
+    'users',
 ]
 
 MIDDLEWARE = [
